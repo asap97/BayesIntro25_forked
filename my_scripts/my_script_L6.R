44f4d066cc185f6a4e9b3599ce009333fa174478
asap97 <as.acevedo@tum.de>
2025-06-04 10:40:42 +0200

    creating my assignmnets folder

--- my_scripts/my_script_L6.R
+++ my_scripts/my_script_L6.R
@@ -1,1 +1,3 @@
-#
+# You need a theoretical argument behind how the world works, the model simply helps you understand it
+# Q: Is finding out a probability distribution that best fits the data a model..? 
+# Q: The prior of the parameters assumes a probability or a probability distribution? Is the prior a number or a distribution? --> A: it is a probability distribution over the parameters before having seen the data
